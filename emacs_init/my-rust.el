386c018edba3dbf6e2c2d6de5bef8c2f37824129
wfj <wufangjie1223@126.com>
2021-11-11 22:02:42 +0800

    add feature: crate level grep

--- emacs_init/my-rust.el
+++ emacs_init/my-rust.el
@@ -38,13 +38,29 @@
 	  (rustic-compilation c (list :buffer buf :process proc :mode mode)))
       (message "Could not find test at point."))))
 
+(defun rustic-crate-grep ()
+  (interactive)
+  (let ((dir (locate-dominating-file
+	      (buffer-file-name (current-buffer)) "Cargo.toml")))
+    (if dir
+	(progn
+	  (let ((buffer (get-buffer-create "*rust grep*")))
+	    (with-current-buffer buffer (read-only-mode -1) (erase-buffer))
+	    (call-process-shell-command
+	     (format "grep -rn \"%s\" %s --exclude-dir=target/ --exclude-dir=.git/"
+		     (read-from-minibuffer "Content to grep: ") dir)
+	     nil buffer)
+	    (with-current-buffer buffer (insert "\nGrep finished!") (grep-mode))
+	    (pop-to-buffer buffer))))
+    (message "Can not find crate root!")))
+
 (add-hook 'rustic-mode-hook
 	  (lambda ()
 	    (local-unset-key (kbd "C-c C-c C-t"))
 	    (local-set-key (kbd "C-c C-c C-t") 'rustic-cargo-fmt-and-test)
 	    (local-unset-key (kbd "C-c C-c C-c"))
 	    (local-set-key (kbd "C-c C-c C-c") 'rustic-cargo-fmt-and-current-test)
+	    (local-set-key (kbd "C-c C-c C-g") 'rustic-crate-grep)
 	    ))
 
-;; the C-c C-c prefix seems foolish
 (provide 'my-rust)
