8e05e48bcb0f6831ba695fea56e2d418e06bdef6
wfj <wufangjie1223@126.com>
2022-03-21 20:59:01 +0800

    1. integrate pyenv, 2. improve C-c C-c for python-mode on emacs@28

--- emacs_init/my-python.el
+++ emacs_init/my-python.el
@@ -9,6 +9,10 @@
 (if (string-equal system-type "gnu/linux")
     (setq python-shell-interpreter "python3"))
 
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+
 ;; (add-hook 'python-mode-hook 'hs-minor-mode)
 (add-hook 'inferior-python-mode-hook
 	  (lambda ()
@@ -50,12 +54,14 @@
 ;;     (eval-print-last-sexp)))
 ;; (el-get 'sync)
 
+;;; used to add custom path TODO: those pth already in site-packages
 ;; (setq jedi:server-args
 ;;       '("--sys-path" "/usr/local/lib/python3.8/dist-packages"
 ;; 	"--sys-path" "/usr/lib/python3/dist-packages"
 ;; 	"--sys-path" "/usr/lib/python3.8/dist-packages"
 ;; 	;"--sys-path" "~/packages"
 ;; 	))
+
 
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
@@ -65,6 +71,53 @@
 ;; NOTE: Do not add jedi to interpreter, pdb single character complete!
 ;; (add-hook 'inferior-python-mode-hook 'jedi:setup)
 
+
+;; #####################################################################
+;; smart C-c C-c
+;; #####################################################################
+(defun python-shell-send-buffer-smart (&optional send-main)
+  (interactive (list current-prefix-arg))
+  (unless (python-shell-get-process)
+    (let* ((current-prefix-arg t)
+	   (process (call-interactively 'run-python)))
+      (comint-send-string
+       process
+       (format "exec(%s)\n" (python-shell--encode-string
+			     python-shell-eval-setup-code)))))
+  (python-shell-send-buffer send-main))
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (local-unset-key (kbd "C-c C-c"))
+	    (local-set-key (kbd "C-c C-c") 'python-shell-send-buffer-smart)))
+
+
+;; #####################################################################
+;; `pyenv'
+;; #####################################################################
+(defun pyenv-shell (version)
+  (interactive (list (completing-read "Pyenv: " (pyenv-versions))))
+  (setenv "PYENV_VERSION" version)
+  (setq python-shell-interpreter (pyenv-full-path version))
+  )
+
+(defun pyenv-shell-unset ()
+  (interactive)
+  (setenv "PYENV_VERSION"))
+
+(defun pyenv-versions ()
+  (cons "system" (split-string (shell-command-to-string "pyenv versions --bare"))))
+
+(defun pyenv-version ()
+  (interactive)
+  (message (or (getenv "PYENV_VERSION") "system")))
+
+(defun pyenv-full-path (version)
+  (if (string= version "system")
+      "python3"
+    (concat (string-trim-right (shell-command-to-string "pyenv root"))
+	    "/versions/"
+	    version
+	    "/bin/python3")))
 
 
 (provide 'my-python)
