ef739485026a569c6e7e07b076935bd212eb1be5
wfj <wufangjie1223@126.com>
2022-03-28 21:53:45 +0800

    a temporary simple replacement of kuma's web search functionality

---
+++ emacs_init/my-kuma.el
@@ -0,0 +1,49 @@
+;;; my-kuma.el -*- lexical-binding: t -*-
+
+(defvar web-open-command
+  (cond ((string= system-type "gnu/linux") "xdg-open")
+	((string= system-type "darwin") "open")
+	((string= system-type "windows-nt") "call") ;; TODO: try
+	(t nil)))
+
+(let ((pairs
+       '(("bing" . "https://www.bing.com/search?q=%s")
+	 ("dict(bing)" . "https://cn.bing.com/dict/search?q=%s")
+	 ("google" . "https://www.google.com/search?q=%s")
+	 ("scholar(google)" . "https://scholar.google.com/scholar?q=%s")
+	 ("stackoverflow" . "https://stackoverflow.com/search?q=%s")
+	 ("wiki" . "https://en.wikipedia.org/wiki/%s")
+	 ("douban" . "https://www.douban.com/search?q=%s")
+	 ("book(douban)" . "https://book.douban.com/subject_search?search_text=%s&cat=1001")
+	 ("movie(douban)" . "https://movie.douban.com/subject_search?search_text=%s&cat=1002")
+	 ("github" . "https://github.com/search?q=%s")
+	 ("jingdong" . "https://search.jd.com/Search?keyword=%s&enc=utf-8")
+	 ("taobao" . "https://s.taobao.com/search?q=%s")
+	 ("zhihu" . "https://www.zhihu.com/search?type=content&q=%s")
+	 ("baidu" . "https://www.baidu.com/s?wd=%s")
+	 ("pythonlibs" . "https://www.lfd.uci.edu/~gohlke/pythonlibs/")
+	 ("pypi" . "https://pypi.python.org/search/?q=%s"))))
+  (let ((dict (make-hash-table :test 'equal)))
+    (dolist (pair pairs)
+      (puthash (car pair) (cdr pair) dict))
+    (defun kuma ()
+      (interactive)
+      (let ((content (gethash (completing-read "Search (web): " dict) dict)))
+	(if content
+	    (if web-open-command
+		(call-process-shell-command
+		 (format "%s \"%s\""
+			 web-open-command
+			 ;; TODO: urlencode
+			 (format content
+				 (read-from-minibuffer "Keyword: ")))
+		 nil 0)
+	      (message "Unknown system!")
+	  (message "Unknown type!"))
+	  )))))
+
+
+(global-set-key (kbd "C-;") 'kuma)
+
+
+(provide 'my-kuma)
