a5567139a1e30d9fe889a5454a6ae2fd332d6a4e
wfj <wufangjie1223@126.com>
2022-07-03 10:54:22 +0800

    remove useless rust-std search (use kuma' rust-std instead), and process newlines

--- emacs_init/my-kuma.el
+++ emacs_init/my-kuma.el
@@ -6,6 +6,20 @@
 	((string= system-type "windows-nt") "call") ;; TODO: try
 	(t nil)))
 
+;; TODO: change doc from nightly to stable, but there is some issue for m1
+;; https://github.com/rust-lang/rustup/issues/2692
+(defun rust-search-std-locally ()
+  (interactive)
+  (call-process-shell-command
+   (format "osascript -e 'tell application \"safari\" to open location \"file:///Users/wufangjie/.rustup/toolchains/nightly-aarch64-apple-darwin/share/doc/rust/html/std/index.html?search=%s\"'"
+	   (read-from-minibuffer "Keyword: "))
+   nil 0)
+  (call-process-shell-command "open -a safari" nil 0)
+  )
+
+(global-set-key (kbd "C-'") 'rust-search-std-locally)
+
+
 (let ((pairs
        '(("bing" . "https://www.bing.com/search?q=%s")
 	 ("dict(bing)" . "https://cn.bing.com/dict/search?q=%s")
@@ -22,7 +36,8 @@
 	 ("zhihu" . "https://www.zhihu.com/search?type=content&q=%s")
 	 ("baidu" . "https://www.baidu.com/s?wd=%s")
 	 ("pythonlibs" . "https://www.lfd.uci.edu/~gohlke/pythonlibs/")
-	 ("pypi" . "https://pypi.python.org/search/?q=%s"))))
+	 ("pypi" . "https://pypi.python.org/search/?q=%s")
+	 )))
   (let ((dict (make-hash-table :test 'equal)))
     (dolist (pair pairs)
       (puthash (car pair) (cdr pair) dict))
@@ -35,8 +50,7 @@
 		 (format "%s \"%s\""
 			 web-open-command
 			 ;; TODO: urlencode
-			 (format content
-				 (read-from-minibuffer "Keyword: ")))
+			 (format content (read-from-minibuffer "Keyword: ")))
 		 nil 0)
 	      (message "Unknown system!"))
 	  (message "Unknown type!"))
